d1fe5a4fbfccd84e10bc534f2ff3b801039308c6
Danny Tran <danny.tran@digisynd.com>
2011-12-02 12:40:36 -0600

    Adding minified and non-minified versions.

--- src/build.rb
+++ src/build.rb
@@ -4,4 +4,6 @@ require 'rubygems'
 require 'uglifier'
 require 'coffee-script'
 
-File.open("../lib/XTND.js", 'w') { |f| f.write  Uglifier.compile(CoffeeScript.compile(File.read("XTND.js.coffee"))) }
+javascript = CoffeeScript.compile(File.read("XTND.js.coffee"))
+File.open("../lib/XTND.js",'w') { |f| f.write javascript }
+File.open("../lib/XTND.min.js", 'w') { |f| f.write  Uglifier.compile(javascript) }
